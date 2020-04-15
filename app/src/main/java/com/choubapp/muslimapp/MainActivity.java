6966864431f26bacf5bb550c626f0f6ea493d27f
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2020-04-15 18:50:24 +0100

    activity_main edited, fonts&strings (arabic) added

--- app/src/main/java/com/choubapp/muslimapp/MainActivity.java
+++ app/src/main/java/com/choubapp/muslimapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.choubapp.muslimapp;
 
-import android.annotation.SuppressLint;
-import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,5 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+       }
 }
